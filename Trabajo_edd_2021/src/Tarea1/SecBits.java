ddddd1a0365c667b045fca539acd038c8d0e21fa
ignac <ignac@LAPTOP-31QCQ582>
2021-04-18 20:49:52 -0400

    añadi metodos On y Off

--- Trabajo_edd_2021/src/Tarea1/SecBits.java
+++ Trabajo_edd_2021/src/Tarea1/SecBits.java
@@ -12,4 +12,19 @@ public class SecBits {
         sec=new short[n/16];
     }
 
+    public void On(int i, int j){
+        for(int a=0;a<sec.length;a++){
+            if(i<=a || j>=a){
+                sec[a]=1;
+            }
+        }
+    }
+    
+    public void Off(int i, int j){
+        for(int a=0;a<sec.length;a++){
+            if(i<=a || j>=a){
+                sec[a]=0;
+            }
+        }
+    }
 }
