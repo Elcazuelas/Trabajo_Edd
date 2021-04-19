bcde92853578dc3697afecbdf171af69fb45d67b
El cazuelas <julio.insuco@gmail.com>
2021-04-18 20:41:14 -0400

    xd

--- Trabajo_edd_2021/src/Tarea1/SecBits.java
+++ Trabajo_edd_2021/src/Tarea1/SecBits.java
@@ -5,9 +5,11 @@ package Tarea1;
  * @author Julio Monroy - Camilo Vazques
  */
 public class SecBits {
-
+    
+    private short[] sec;
+    
     public SecBits(int n) {
         n=0;
     }
-    
+
 }
