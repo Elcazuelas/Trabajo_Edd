bc01f920c04bf9c6414d7690afc13e31426d80c7
El cazuelas <julio.insuco@gmail.com>
2021-04-18 23:20:32 -0400

    xd

--- Trabajo_edd_2021/src/Tarea1/SecBits.java
+++ Trabajo_edd_2021/src/Tarea1/SecBits.java
@@ -6,17 +6,32 @@ package Tarea1;
  */
 public class SecBits {
 
-    private short[] sec;
+    private short[] sec;// 010000000000 0000000000000000 [0 ,0 ]
 
-    public SecBits(int n) {
+    public SecBits(int n) { //
         sec = new short[n / 16];
     }
 
     public void On(int i, int j) {
-        for (int a = 0; a < sec.length; a++) { //recorre 1 por 1 los bits
-            if (i <= a || j >= a) {   //busca la posicion de enmedio de los dos datos dados
-                sec[a] = 1;       //reeemplaza por 1
+        int msk = 0x80000000;//1000000..
+        int aux=0;
+        int ms = 0x80000000; // m´ascara (en hexadecimal)
+        for (short k = 0; k < sec.length*16; k++) { //recorre 1 por 1 los bits
+            if (i <= k && j >= k) {   //busca la posicion de enmedio de los dos datos dados
+                sec[aux]=(short) (sec[aux] | msk);
             }
+            msk=(short) (msk>>>1);
+            
+            
+            for (short l = 0; l < 16; l++) {
+            if ((ms & msk) == 0){ 
+                System.out.print("0");
+            }else {System.out.print("1");}
+        ms = (short) (ms >>> 1);
+        
+        }
+            System.out.println("");
+            ms = (short) 0x8000;
         }
     }
 
@@ -61,12 +76,23 @@ public class SecBits {
         }
         return true;
     }
-
+    /*
     public void Print(int i, int j) {
         for (int a = 0; a < sec.length; a++) {
             if (i <= a || j >= a) {
                 System.out.print(a + " ");
             }
+        }
+    }*/
+    public void intTobit() {
+        short msk = (short) 0x8000; // m´ascara (en hexadecimal)
+        int aux=0;
+        for (short i = 0; i < 16; i++) {
+            if ((sec[aux] & msk) == 0){ 
+                System.out.print("0");
+            }else {System.out.print("1");}
+        msk = (short) (msk >>> 1);
+        
         }
     }
 }
---
+++ Trabajo_edd_2021/src/Tarea1/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Tarea1;
+
+/**
+ * 
+ * @author Julio Monroy - Camilo Vazques
+ */
+public class Test {
+    public static void main(String[] args) {
+        SecBits td=new SecBits(16);
+        td.On(0, 4);
+        td.intTobit();
+    }
+
+}
