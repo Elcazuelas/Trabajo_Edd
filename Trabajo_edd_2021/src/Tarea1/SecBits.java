49170d7568c14a16dca29f38c9b4330c01b1a745
ignac <ignac@LAPTOP-31QCQ582>
2021-04-18 21:09:53 -0400

    añadi metodos BitsOn, SelPos y SubsecuenciasSimilares

--- Trabajo_edd_2021/src/Tarea1/SecBits.java
+++ Trabajo_edd_2021/src/Tarea1/SecBits.java
@@ -13,9 +13,9 @@ public class SecBits {
     }
 
     public void On(int i, int j){
-        for(int a=0;a<sec.length;a++){
-            if(i<=a || j>=a){
-                sec[a]=1;
+        for(int a=0;a<sec.length;a++){ //recorre 1 por 1 los bits
+            if(i<=a || j>=a){   //busca la posicion de enmedio de los dos datos dados
+                sec[a]=1;       //reeemplaza por 1
             }
         }
     }
@@ -26,5 +26,39 @@ public class SecBits {
                 sec[a]=0;
             }
         }
+    }
+    
+    public int BitsOn(int j){
+        int cont=0;
+        for(int a=0;a<sec.length;a++){
+            if(sec[a]==1){
+                cont++;
+            }
+        }
+        return cont;
+    }
+    
+    public int SelPos(int l){
+        int cont=0;
+        for(int a=0;a<sec.length;a++){
+            if(sec[a]==1){
+                cont++;
+                if(l==cont){
+                    return a;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    public boolean SubsecuenciaSimilares(SecBits s, int i,int j){
+        for(int a=0;a<sec.length;a++){
+            if(i<=a || j>=a){
+                if(!(s.sec[a]==this.sec[a])){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
