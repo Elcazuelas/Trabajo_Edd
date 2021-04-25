740db0a594b8bb2d250fb987652d478a313ac3aa
El cazuelas <julio.insuco@gmail.com>
2021-04-25 15:33:06 -0400

    está bueno el on

--- Trabajo_edd_2021/src/Tarea1/Test.java
+++ Trabajo_edd_2021/src/Tarea1/Test.java
@@ -12,8 +12,22 @@ package Tarea1;
  */
 public class Test {
     public static void main(String[] args) {
-        SecBits td=new SecBits(16);
-        td.On(0, 4);
+        SecBits td=new SecBits(128);
+        SecBits s = new SecBits(64);
+        td.on(1,5);
+        s.on(1,6);
+        td.on(32,33 );
+        s.on(32,33 );
+        if (td.SubsecuenciaSimilares(s, 1,6)) {
+            System.out.println("sisoy");
+        }else{
+            System.out.println("nosoy");
+        }
+        //td.off(0,64);
+        System.out.println(td.BitsOn(126));
+        System.out.println(td.SelPos(6));
+        td.Print(0, 35);
+
         td.intTobit();
     }
 
